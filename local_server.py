f45e4a9c94da9feaaa983cb74d5274978d4da687
tkn <ivoshl.art@gmail.com>
2019-02-19 01:38:33 -0500

    added local server script + source in html

---
+++ local_server.py
@@ -0,0 +1,10 @@
+import http.server
+import socketserver
+
+if __name__ == '__main__':
+    PORT = 8000
+
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+    print("serving at port", PORT)
+    httpd.serve_forever()
